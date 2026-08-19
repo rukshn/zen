eb1710982f18ea5cc666cd313b5264587d135777
rukshan <arkruka@gmail.com>
2026-08-19 11:26:11 +0200

    feat: :sparkles: switch between conversations

--- src/lib/components/nav-workspaces.svelte
+++ src/lib/components/nav-workspaces.svelte
@@ -1,66 +1,56 @@
 <script lang="ts">
-	import ChevronRightIcon from "@lucide/svelte/icons/chevron-right";
-	import EllipsisIcon from "@lucide/svelte/icons/ellipsis";
-	import PlusIcon from "@lucide/svelte/icons/plus";
-	import * as Collapsible from "$lib/components/ui/collapsible/index.js";
-	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+  import ChevronRightIcon from "@lucide/svelte/icons/chevron-right";
+  import EllipsisIcon from "@lucide/svelte/icons/ellipsis";
+  import PlusIcon from "@lucide/svelte/icons/plus";
+  import * as Collapsible from "$lib/components/ui/collapsible/index.js";
+  import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+  import AbstractInstanceOutlinedIcon from "@iconify-svelte/eos-icons/abstract-instance-outlined";
+  import { getDb } from "@/store/dbstore";
+  import type { Conversation } from "@/store/bases.svelte";
+  import { messageStore } from "@/store/message.svelte";
+	
+  let {
+    sidebarChats,
+  }: { sidebarChats: { firstMessage: string; uuid: string }[] } = $props();
 
-	let {
-		workspaces,
-	}: { workspaces: { name: string; emoji: string; pages: { name: string; emoji: string }[] }[] } =
-		$props();
+  const swtichConversation = async(uuid: string) => {
+	const db = await getDb()
+	const conversation: Conversation[] = await db.select("SELECT * from conversations WHERE uuid = $1" , [uuid])
+
+	if (conversation) {
+		messageStore.activeConversation = uuid
+		messageStore.messages = JSON.parse(conversation[0].messages)
+	}
+  }
 </script>
 
 <Sidebar.Group>
-	<Sidebar.GroupLabel>Workspaces</Sidebar.GroupLabel>
-	<Sidebar.GroupContent>
-		<Sidebar.Menu>
-			{#each workspaces as workspace (workspace.name)}
-				<Collapsible.Root>
-					<Sidebar.MenuItem>
-						<Sidebar.MenuButton>
-							{#snippet child({ props })}
-								<a href="##" {...props}>
-									<span>{workspace.emoji}</span>
-									<span>{workspace.name}</span>
-								</a>
-							{/snippet}
-						</Sidebar.MenuButton>
-						<Collapsible.Trigger>
-							{#snippet child({ props })}
-								<Sidebar.MenuAction
-									{...props}
-									class="start-2 bg-sidebar-accent text-sidebar-accent-foreground data-[state=open]:rotate-90"
-									showOnHover
-								>
-									<ChevronRightIcon />
-								</Sidebar.MenuAction>
-							{/snippet}
-						</Collapsible.Trigger>
-						<Sidebar.MenuAction showOnHover>
-							<PlusIcon />
-						</Sidebar.MenuAction>
-						<Collapsible.Content>
-							<Sidebar.MenuSub>
-								{#each workspace.pages as page (page.name)}
-									<Sidebar.MenuSubItem>
-										<Sidebar.MenuSubButton href="##">
-											<span>{page.emoji}</span>
-											<span>{page.name}</span>
-										</Sidebar.MenuSubButton>
-									</Sidebar.MenuSubItem>
-								{/each}
-							</Sidebar.MenuSub>
-						</Collapsible.Content>
-					</Sidebar.MenuItem>
-				</Collapsible.Root>
-			{/each}
-			<Sidebar.MenuItem>
-				<Sidebar.MenuButton class="text-sidebar-foreground/70">
-					<EllipsisIcon />
-					<span>More</span>
-				</Sidebar.MenuButton>
-			</Sidebar.MenuItem>
-		</Sidebar.Menu>
-	</Sidebar.GroupContent>
+  <Sidebar.GroupLabel>Converations</Sidebar.GroupLabel>
+  <Sidebar.GroupContent>
+    <Sidebar.Menu>
+      {#each sidebarChats as chat (chat.uuid)}
+        <Collapsible.Root>
+          <Sidebar.MenuItem>
+            <Sidebar.MenuButton onclick={() => swtichConversation(chat.uuid)}>
+              {#snippet child({ props })}
+                <a href="##" {...props}>
+                  <span><AbstractInstanceOutlinedIcon /></span>
+                  <span>{chat.firstMessage}</span>
+                </a>
+              {/snippet}
+            </Sidebar.MenuButton>
+            <Sidebar.MenuAction showOnHover>
+              <PlusIcon />
+            </Sidebar.MenuAction>
+          </Sidebar.MenuItem>
+        </Collapsible.Root>
+      {/each}
+      <Sidebar.MenuItem>
+        <Sidebar.MenuButton class="text-sidebar-foreground/70">
+          <EllipsisIcon />
+          <span>More</span>
+        </Sidebar.MenuButton>
+      </Sidebar.MenuItem>
+    </Sidebar.Menu>
+  </Sidebar.GroupContent>
 </Sidebar.Group>
